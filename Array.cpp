286710d50256f8f3ce64533d19c3fb2f40da651c
Vicky Dewangan <vickydewangan581@gmail.com>
2024-01-23 20:35:13 +0530

    first commit

---
+++ Array.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[4];
+    for (int i = 0; i < 4; i++)
+    {
+        cin>>arr[i];
+    }
+    cout<<"Array";
+    for ( int i = 0; i < 4; i++)
+    {
+        cout<<arr[i]<<endl;
+    }
+    
+    return 0;
+}
